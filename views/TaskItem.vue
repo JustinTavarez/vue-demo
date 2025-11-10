db429395fc05957d68068546a721bfa7f43e545d
Justin Tavarez <62660253+JustinTavarez@users.noreply.github.com>
2025-11-10 14:45:12 -0500

    Add TaskItem component for task management

---
+++ views/TaskItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li :class="['task-item', { completed: task.done }]">
+    <input 
+      type="checkbox"
+      class="task-checkbox"
+      :checked="task.done"
+      @change="$emit('toggle', task.id)"
+    >
+    <span class="task-text">{{ task.text }}</span>
+    <button class="task-delete" @click="$emit('delete', task.id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    task: { type: Object, required: true }
+  },
+  emits: ['toggle', 'delete']
+};
+</script>
